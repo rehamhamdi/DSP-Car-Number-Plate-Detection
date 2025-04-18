34dc59ee721fbe4d3ed5340bc6f9a46fa85945f0
Ziad Mohammed <00ziadmohammed606@gmail.com>
2025-04-18 21:09:08 +0200

    data collection and noise reduction

---
+++ preprocessing.m
@@ -0,0 +1,33 @@
+pkg load image
+
+originalImageName = 'angeled2.jpg';
+filteredImageName = 'angeled2_filtered.jpg';
+
+
+scriptFolder = fileparts(mfilename('fullpath'));
+originalBaseFolder = fullfile(scriptFolder, 'LP_Samples', 'originalImages');
+originalImagePath = fullfile(originalBaseFolder, originalImageName);
+
+originalImage = imread(originalImagePath);
+
+if size(originalImage, 3) == 3
+    grayImage = rgb2gray(originalImage);
+else
+    grayImage = originalImage;  % already grayscale
+end
+
+% Histogram equalization
+enhancedImage = histeq(grayImage);
+
+% OR, use contrast stretching
+adjustedImage = imadjust(grayImage);
+
+% Gaussian filter
+filteredImage = imgaussfilt(enhancedImage, 1);  % sigma = 1
+
+
+filteredBaseFolder = fullfile(scriptFolder, 'LP_Samples', 'filteredImages');
+filteredImagePath = fullfile(filteredBaseFolder, filteredImageName);
+imwrite(filteredImage, filteredImagePath);
+
+
